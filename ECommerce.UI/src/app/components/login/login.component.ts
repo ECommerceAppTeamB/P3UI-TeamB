69607862b34cf14334cc734a4c348bbfd1fe2e44
Brianna Renni <briannaxrene@icloud.com>
2022-12-27 13:51:43 -0800

    (Feature): Add validation to login email input

--- ECommerce.UI/src/app/components/login/login.component.ts
+++ ECommerce.UI/src/app/components/login/login.component.ts
@@ -1,38 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import {
+    FormBuilder,
+    UntypedFormControl,
+    UntypedFormGroup,
+    Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+    loginForm = new UntypedFormGroup({
+        email: new UntypedFormControl('', [
+            Validators.required,
+            Validators.email,
+        ]),
+        password: new UntypedFormControl('', Validators.required),
+    });
 
-  loginForm = new UntypedFormGroup({
-    email: new UntypedFormControl(''),
-    password: new UntypedFormControl('')
-  })
-  
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private fb: FormBuilder
+    ) {}
 
-  constructor(private authService: AuthService, private router: Router) { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-  
-  onSubmit(): void {
-    this.authService.login(this.loginForm.get('email')?.value, this.loginForm.get('password')?.value).subscribe(
-      () => {
-        this.authService.loggedIn=true;
-      },
-      (err) => console.log(err),
-      () => this.router.navigate(['home'])
-    );
-  }
+    onSubmit(): void {
+        this.loginForm = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required],
+        });
+        this.authService
+            .login(
+                this.loginForm.get('email')?.value,
+                this.loginForm.get('password')?.value
+            )
+            .subscribe(
+                () => {
+                    this.authService.loggedIn = true;
+                },
+                (err) => console.log(err),
+                () => this.router.navigate(['home'])
+            );
+    }
 
-  register(): void {
-    this.router.navigate(['register']);
-  }
-
+    register(): void {
+        this.router.navigate(['register']);
+    }
 }
--- ECommerce.UI/src/app/components/register/register.component.ts
+++ ECommerce.UI/src/app/components/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import {
+    FormBuilder,
+    UntypedFormControl,
+    UntypedFormGroup,
+    Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -16,13 +21,13 @@ export class RegisterComponent implements OnInit {
     email: new UntypedFormControl(''),
     password: new UntypedFormControl('')
   })
-  
+
 
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
-  
+
   onSubmit(): void {
     this.authService.register(this.registerForm.get('fname')?.value, this.registerForm.get('lname')?.value, this.registerForm.get('email')?.value, this.registerForm.get('password')?.value).subscribe(
       () => console.log("New user registered"),
