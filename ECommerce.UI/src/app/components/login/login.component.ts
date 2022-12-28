1dd1b0c3f5cb70988f1256188f61f07c568630e1
Brianna Renni <briannaxrene@icloud.com>
2022-12-27 22:11:49 -0800

    (Feature): Add validation to register and checkout

--- ECommerce.UI/src/app/components/login/login.component.ts
+++ ECommerce.UI/src/app/components/login/login.component.ts
@@ -1,55 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import {
-    FormBuilder,
-    UntypedFormControl,
-    UntypedFormGroup,
-    Validators,
-} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css'],
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css'],
 })
+
 export class LoginComponent implements OnInit {
-    loginForm = new UntypedFormGroup({
-        email: new UntypedFormControl('', [
-            Validators.required,
-            Validators.email,
-        ]),
-        password: new UntypedFormControl('', Validators.required),
-    });
+	loginForm: FormGroup = this.fb.group({});
 
-    constructor(
-        private authService: AuthService,
-        private router: Router,
-        private fb: FormBuilder
-    ) {}
+	constructor(private authService: AuthService, private router: Router, private fb: FormBuilder) {}
 
-    ngOnInit(): void {}
+	ngOnInit() {
+		this.loginForm = this.fb.group({
+			email: ['', [Validators.required, Validators.email], { updateOn: 'blur' }],
+			password: ['', Validators.required],
+		});
+	}
 
-    onSubmit(): void {
-        this.loginForm = this.fb.group({
-            email: ['', [Validators.required, Validators.email]],
-            password: ['', Validators.required],
-        });
-        this.authService
-            .login(
-                this.loginForm.get('email')?.value,
-                this.loginForm.get('password')?.value
-            )
-            .subscribe(
-                () => {
-                    this.authService.loggedIn = true;
-                },
-                (err) => console.log(err),
-                () => this.router.navigate(['home'])
-            );
-    }
+	onSubmit(): void {
+		const email = this?.loginForm?.get('email')?.value;
+		const password = this?.loginForm?.get('password')?.value;
+		this.authService.login(email, password).subscribe(
+			() => {this.authService.loggedIn = true;},
+			(err) => console.log(err),
+			() => this.router.navigate(['home'])
+		);
+	};
 
-    register(): void {
-        this.router.navigate(['register']);
-    }
+	register(): void {
+		this.router.navigate(['register']);
+	}
 }
--- ECommerce.UI/src/app/components/register/register.component.ts
+++ ECommerce.UI/src/app/components/register/register.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-    FormBuilder,
-    UntypedFormControl,
-    UntypedFormGroup,
-    Validators,
-} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -14,22 +9,25 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = this.fb.group({});
 
-  registerForm = new UntypedFormGroup({
-    fname: new UntypedFormControl(''),
-    lname: new UntypedFormControl(''),
-    email: new UntypedFormControl(''),
-    password: new UntypedFormControl('')
-  })
+  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder) { }
 
-
-  constructor(private authService: AuthService, private router: Router) { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      fname: ['', Validators.required],
+      lname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email], { updateOn: 'blur' }],
+      password: ['', Validators.required],
+    });
   }
 
   onSubmit(): void {
-    this.authService.register(this.registerForm.get('fname')?.value, this.registerForm.get('lname')?.value, this.registerForm.get('email')?.value, this.registerForm.get('password')?.value).subscribe(
+    const fname = this?.registerForm?.get('fname')?.value;
+    const lname = this?.registerForm?.get('lname')?.value;
+    const email = this?.registerForm?.get('email')?.value;
+    const password = this?.registerForm?.get('password')?.value;
+    this.authService.register(fname, lname, email, password).subscribe(
       () => console.log("New user registered"),
       (err) => console.log(err),
       () => this.router.navigate(['login'])
