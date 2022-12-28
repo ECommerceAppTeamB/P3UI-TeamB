b5494aab02d095dabbf923f2923c486aa235f10a
Brianna Renni <briannaxrene@icloud.com>
2022-12-28 10:39:49 -0800

    Fix https URL

--- ECommerce.UI/src/environments/environment.ts
+++ ECommerce.UI/src/environments/environment.ts
@@ -9,17 +9,6 @@ export const environment = {
   baseUrl: "https://localhost:7078",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Access-Control-Allow-Origin': 'https://localhost:4200',
   },
 };
-
-
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
