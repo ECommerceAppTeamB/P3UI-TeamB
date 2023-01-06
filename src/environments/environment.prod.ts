376f022d086e564dc83fc1b4700608ae4a120e18
Bryon F <bryon790@revature.net>
2023-01-06 14:07:24 -0600

    re add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   baseUrl: "https://tentacles-shopping-api.azurewebsites.net",
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin':'https://ecommerceappteamb.github.io'
   },
 };
