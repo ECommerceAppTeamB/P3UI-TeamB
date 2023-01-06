66e09bee0ed31acb2b456a2fed447ecf11035c93
Bryon F <bryon790@revature.net>
2023-01-06 14:22:27 -0600

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   baseUrl: "https://tentacles-shopping-api.azurewebsites.net",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'https://ecommerceappteamb.github.io/P3UI-TeamB/',
+    'Access-Control-Allow-Origin': 'https://ecommerceappteamb.github.io/P3UI-TeamB/', 
   },
 };
