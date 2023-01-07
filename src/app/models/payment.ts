7877af66d5af83faa54bea8013d3d31cdd36ddef
Bryon F <bryon790@revature.net>
2023-01-06 19:20:07 -0600

    Auto-generated commit [ci skip]

--- src/app/models/payment.ts
+++
@@ -1,9 +0,0 @@
-export class Payment {
-    name: string
-    detail: string
-
-    constructor (name: string, detail: string) {
-        this.name = name;
-        this.detail = detail;
-    }
-}
--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: "https://tentacles-shopping-api.azurewebsites.net",
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'https://ecommerceappteamb.github.io/P3UI-TeamB/', 
-  },
-};
