d19de612d3fc0669db9801d0203c3736c445311e
Brianna Renni <briannaxrene@icloud.com>
2023-01-06 16:45:41 -0800

    separate environment.prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,8 @@
-// Build
-// export const environment = {
-//   production: false,
-//   baseUrl: "https://tentacles-shopping-api.azurewebsites.net",
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Access-Control-Allow-Origin': 'https://localhost:4200',
-//   },
-// };
-
-
-// Deploy
 export const environment = {
-  production: true,
+  production: false,
   baseUrl: "https://tentacles-shopping-api.azurewebsites.net",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'https://ecommerceappteamb.github.io/P3UI-TeamB/',
+    'Access-Control-Allow-Origin': 'https://localhost:4200',
   },
 };
